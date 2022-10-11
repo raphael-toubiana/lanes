d05e8fe08d582f6de8948a5f30588a289537f8c8
Raphael Toubiana <raphael.toubiana@gmail.com>
2022-10-11 17:39:16 +0200

    page about added

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  get '/about', to: 'pages#about', as: 'about' 
   root to: 'trips#index'
   resources :trips, only: [:new, :create, :edit, :update, :destroy, :show] do
     resources :reviews, only: [:new, :create]
