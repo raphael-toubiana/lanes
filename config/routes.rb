1cc7ac70a9973beffba0d9f90f4036adfafd5dca
Raphael Toubiana <raphael.toubiana@gmail.com>
2022-10-12 12:42:03 +0200

    routes reordering

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
-  get '/about', to: 'pages#about', as: 'about' 
   root to: 'trips#index'
+  get '/about', to: 'pages#about', as: 'about' 
   resources :trips, only: [:new, :create, :edit, :update, :destroy, :show] do
     resources :reviews, only: [:new, :create]
     collection do
