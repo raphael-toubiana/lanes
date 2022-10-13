999cfde80e1100adc9acd69889cab0301afc0b62
Raphael Toubiana <raphael.toubiana@gmail.com>
2022-10-13 15:11:31 +0200

    routes refacto

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   root to: 'trips#index'
-  get '/about', to: 'pages#about', as: 'about' 
-  resources :trips, only: [:new, :create, :edit, :update, :destroy, :show] do
+  get '/about', to: 'pages#about', as: 'about'
+  resources :trips, except: [:index] do
     resources :reviews, only: [:new, :create]
     collection do
       get :hard
