90bc971a1ba680a1c4775f4a36888587a7dfcea1
Raphael Toubiana <raphael.toubiana@gmail.com>
2022-10-14 16:37:54 +0200

    add validations on reviews

--- app/models/review.rb
+++ app/models/review.rb
@@ -1,6 +1,6 @@
 class Review < ApplicationRecord
   belongs_to :trip
 
-  validates :rating, presence: true, numericality: true
-  validates :content, presence: true
+  validates :rating, presence: true, numericality: { less_than_or_equal_to: 10,  only_integer: true }
+  validates :content, presence: true, length: {minimum: 20 }
 end
