0b4b57f9e58fe37ea5e59971db5ad2c98b9a5b91
Raphael Toubiana <raphael.toubiana@gmail.com>
2022-10-19 14:44:33 +0200

    indent

--- app/models/review.rb
+++ app/models/review.rb
@@ -2,5 +2,5 @@ class Review < ApplicationRecord
   belongs_to :trip
 
   validates :rating, presence: true, numericality: { less_than_or_equal_to: 10,  only_integer: true }
-  validates :content, presence: true, length: {minimum: 20 }
+  validates :content, presence: true, length: { minimum: 20 }
 end
