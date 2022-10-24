ecdfe3d091c18082036271308e3c848e974313aa
Raphael Toubiana <raphael.toubiana@gmail.com>
2022-10-24 16:36:38 +0200

    migration rollback

--- db/migrate/20221024142500_add_coordinates_to_trips.rb
+++
@@ -1,8 +0,0 @@
-class AddCoordinatesToTrips < ActiveRecord::Migration[7.0]
-  def change
-    add_column :trips, :starting_latitude, :float
-    add_column :trips, :starting_longitude, :float
-    add_column :trips, :ending_latitude, :float
-    add_column :trips, :ending_longitude, :float
-  end
-end
