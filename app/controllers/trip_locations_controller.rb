c78d760b99a58ca0802f75b90f00774467430dae
Raphael Toubiana <raphael.toubiana@gmail.com>
2022-10-28 15:28:05 +0200

    edit/update on trip locations

--- app/controllers/trip_locations_controller.rb
+++ app/controllers/trip_locations_controller.rb
@@ -1,12 +1,15 @@
 class TripLocationsController < ApplicationController
   before_action :set_trip, only: [:index, :new, :create]
-
-  def index
-    @trip_locations = @trip.trip_locations
-  end
+  before_action :set_trip_location, only: [:edit, :update]
 
   def new
-    @trip_location = TripLocation.new
+    if params[:starting_location]
+      @trip_location = TripLocation.new(starting_location: params[:starting_location])
+    elsif params[:target_location]
+      @trip_location = TripLocation.new(target_location: params[:target_location])
+    else
+      @trip_location = TripLocation.new
+    end
     @location = Location.new
   end
 
@@ -22,10 +25,28 @@ class TripLocationsController < ApplicationController
     end
   end
 
+  def edit
+  end
+
+  def update
+    @location = @trip_location.location
+    @location.update(address: params[:location][:address])
+    @location.save!
+    if @trip_location.save
+        redirect_to trip_path(@trip_location.trip)
+    else
+        render :edit, status: :unprocessable_entity
+    end
+  end
+
   private
 
   def set_trip
     @trip = Trip.find(params[:trip_id])
+  end
+
+  def set_trip_location
+    @trip_location = TripLocation.find(params[:id])
   end
 
   def trip_location_params
